ad42f7a6cc796793d4bb6d3a7aa235ac0e9fa2de
mbayangdrame <mb.drame3@isepdiamniadio.edusn>
2026-03-15 13:16:05 +0000

    first commit

---
+++ src/main/java/sn/isi/l3gl/api/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package sn.isi.l3gl.api.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiApplication.class, args);
+    }
+
+}
